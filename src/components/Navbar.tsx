42648bcef104e376ed4913f4fdf731fc4b01a801
AbdelilahGlsid <abdelilah.bouhkka-etu@etu.univh2c.ma>
2024-09-17 20:32:00 +0100

    SideBar MenuItem to Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,25 @@
 import { BookCopy, FileChartPie } from "lucide-react";
 import MenuItem from "./MenuItem";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-      <MenuItem
+      <Link
         to="/"
         className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
       >
         <FileChartPie className="h-5 w-5"/>
         Dashboard
-      </MenuItem>
+      </Link>
 
-      <MenuItem
+      <Link
         to="/matieres"
         className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
       >
         <BookCopy className="h-5 w-5"/>
         Matières
-      </MenuItem>
+      </Link>
     </nav>
   );
 };
