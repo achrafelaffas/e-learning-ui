302147ca91527b57ab3e8efffb68fcfb3ae82ee2
achraf <achrafelaffas@gmail.com>
2024-10-15 23:03:02 +0100

    logo

--- src/pages/Dashboard/Layout.tsx
+++ src/pages/Dashboard/Layout.tsx
@@ -10,7 +10,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { CircleUser, GraduationCap, Menu } from "lucide-react";
+import { CircleUser, Menu } from "lucide-react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import useSignOut from "react-auth-kit/hooks/useSignOut";
 import useAuthUser from "react-auth-kit/hooks/useAuthUser";
@@ -33,13 +33,13 @@ const Lyout = () => {
   };
 
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[150px_1fr] lg:grid-cols-[170px_1fr]">
+    <div className="grid min-h-screen w-full md:grid-cols-[150px_1fr] lg:grid-cols-[190px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link to="/" className="flex items-center gap-2 font-semibold">
-              <GraduationCap />
-              <span className="">E-learning</span>
+              <img src="public\logo.png" width="45px" height="45px" />
+              <span className="">MDS</span>
             </Link>
           </div>
 
