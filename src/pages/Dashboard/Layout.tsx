240b1054c8a34c360ddef5021cbcc12cabe0e681
achraf <achrafelaffas@gmail.com>
2024-10-25 20:19:22 +0100

    Fix Logo Link

--- src/pages/Dashboard/Layout.tsx
+++ src/pages/Dashboard/Layout.tsx
@@ -38,7 +38,10 @@ const Lyout = () => {
       <div className="hidden border-r bg-muted/40 md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-            <Link to="/" className="flex items-center gap-2 font-semibold">
+            <Link
+              to={user?.isAdmin ? "/admin" : "/"}
+              className="flex items-center gap-2 font-semibold"
+            >
               <img src="public\logo.png" width="45px" height="45px" />
               <span className="">MDS</span>
             </Link>
